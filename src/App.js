67617bf7e56e3a320e9163d159064e72e4af38c6
Maximiliano Salguero <you@example.com>
2023-06-08 16:59:24 -0300

    Aula 1 Finalizada - Creando Route

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import  Home  from "./pages/Home.jsx"
+import  Sobre  from "./pages/Sobre.jsx"
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return ( 
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre /> } />
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
