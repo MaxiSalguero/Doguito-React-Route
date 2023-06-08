6313d753c978df815b592d9355eec18e96a8f915
Maximiliano Salguero <you@example.com>
2023-06-08 17:44:01 -0300

    Aula 2 Finalizada - Implementacion: Page404 - Routes , Nav -  Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import  Home  from "./pages/Home.jsx"
-import  Sobre  from "./pages/Sobre.jsx"
-import './assets/css/base/base.css';
+import  Home  from "./pages/Home"
+import  Sobre  from "./pages/Sobre"
+import Page404 from "./pages/Page404"
+import Header from "./components/Header"
+import './assets/css/base/base.css'
 import './assets/css/componentes/card.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
 
   return ( 
     <Router>
+      <Header />
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/sobre" element={<Sobre /> } />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="*" element={<Page404/>} />
         </Routes>
     </Router>
   );
