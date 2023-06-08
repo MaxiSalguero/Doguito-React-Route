d912e18d082c524b5196465cbe2af63a4fa159df
Maximiliano Salguero <you@example.com>
2023-06-08 20:08:22 -0300

    Aula 4 Finalizada - implementacion: listCategories - components, Post.jsx - pages - utilizacion: useNavigate, useEffect, useParams

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import  Home  from "./pages/Home"
 import  Sobre  from "./pages/Sobre"
 import Page404 from "./pages/Page404"
 import Header from "./components/Header"
+import Post from "./pages/Post"
 import './assets/css/base/base.css'
 import './assets/css/componentes/card.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -15,7 +16,9 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
+            <Route path="/posts/:id" element={<Post />} />
             <Route path="*" element={<Page404/>} />
+            <Route path="/posts/*" element={<Page404/>} />
         </Routes>
     </Router>
   );
