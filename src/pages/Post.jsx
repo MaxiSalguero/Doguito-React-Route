1328a2671b80daa94def8111d9b2299f09259b83
Maximiliano Salguero <you@example.com>
2023-06-09 05:42:25 -0300

    Aula 5 finalizada - implementacion: Categoria, Subcategoria - pages,utilizacion: Rutas anidadas, useResolvedPath

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -3,27 +3,29 @@ import "../assets/css/componentes/card.css"
 import { useParams, useNavigate } from "react-router-dom"
 import { buscar } from "../api/api"
 
-const Post = ({url}) => {
- 
-    const [post,setPost] = useState({})
-    //Destructuracion de id
+const Post = ({ url }) => {
+
+    const [post, setPost] = useState({})
+
     const { id } = useParams()
+
     const navigate = useNavigate()
 
     useEffect(() => {
-        buscar(`/posts/${id}`, setPost).catch(()=> {
+        buscar(`/posts/${id}`, setPost).catch(() => {
             navigate("/not-found")
         })
     }, [id])
- 
-    return(
-   <main className = "container flex flex--center">
-     <article className = "card post">
-       <h2 className = "post-card__title">{post.title}</h2>
-       <p className = "text__card">{post.body}</p>
-     </article>
-   </main>
- )
+
+
+    return (
+        <main className="container flex flex--center">
+            <article className="card post">
+                <h2 className="post-card__title">{post.title}</h2>
+                <p className="text__card">{post.body}</p>
+            </article>
+        </main>
+    )
 }
 
 export default Post
